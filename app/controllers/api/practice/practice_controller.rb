77ca7bb66ed16b9a315237d79706c51afc22e539
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-04-01 17:05:30 +0900

    added api endpoint controller

---
+++ app/controllers/api/practice/practice_controller.rb
@@ -0,0 +1,37 @@
+class Api::Practice::PracticeController < ApplicationController
+  def index
+    id = Practice.first.id
+    title = Practice.first.title
+    description = Practice.first.description
+    contents = child_values
+
+    result_values = {
+    id: id,
+    title: title,
+    description: description,
+    contents: contents
+    }
+
+    render json: result_values
+  end
+
+
+  private
+
+  def child_values
+    [
+      {
+        name: "jack",
+        age: 100,
+        introduction: "he is jack"
+      },
+      {
+        name: "bob",
+        age: 100,
+        introduction: "he is bob"
+      }
+    ]
+  end
+
+end
+
