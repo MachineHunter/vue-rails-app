0ed1c0b6953e25689297e598e625f307563c8150
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-04-03 00:11:45 +0900

    rspec succeeded http status code & correct json

---
+++ spec/factories/practice.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :practice do
+    id { 1 }
+    title { "rspec practice title" }
+    description { "rspec practice description" }
+    contents { "rspec first try" }
+  end
+end
