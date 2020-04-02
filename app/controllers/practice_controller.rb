adcbf61aa4c10a5cb89a2d7c977fd07162749cf8
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-04-03 01:52:57 +0900

    written index2 and did routing

--- app/controllers/practice_controller.rb
+++ app/controllers/practice_controller.rb
@@ -1,4 +1,27 @@
 class PracticeController < ApplicationController
   def index
   end
+
+  def index2
+    @title = "Practice3"
+    @description = "Practice without api"
+    @contents = get_contents
+  end
+
+  private
+
+  def get_contents
+    {
+      outer_links: [
+        {
+          name: "Mary",
+          text: "she is mary"
+        },
+        {
+          name: "God",
+          text: "oh my god!!!!"
+        }
+      ]
+    }
+  end
 end
