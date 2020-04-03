28daa3bdd160ce9167954a6cafbe4e711c5230ca
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-04-04 02:16:09 +0900

    added blank page for master to make tables

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 
   get 'practice/index'
   get 'practice/index2'
+  get 'practice/index3'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
