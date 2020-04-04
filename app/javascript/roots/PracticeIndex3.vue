ffa6c7d2fe4a2d11d56a5c6eb3fdc0e9b3a9ed73
Fuga-Kawamata <f.maple.river@gmail.com>
2020-04-04 18:59:41 +0900

    [feat] Implement function to post new data

--- app/javascript/roots/PracticeIndex3.vue
+++ app/javascript/roots/PracticeIndex3.vue
@@ -22,7 +22,7 @@
 
     <div class="popup-cover" v-show="isShownForm">
       <div class="popup-window">
-        <form action="">
+        <form @submit.prevent="registerNewData">
           <div class="form-header">
             <h3 class="form-title">Register New Data</h3>
             <button
@@ -33,15 +33,27 @@
           </div>
           <div class="title">
             <label for="title">title:</label>
-            <input type="text" id="title">
+            <input
+              type="text"
+              id="title"
+              v-model="newPractice.title"
+            >
           </div>
           <div class="description">
             <label for="description">description:</label>
-            <input type="text" id="description">
+            <input
+              type="text"
+              id="description"
+              v-model="newPractice.description"
+            >
           </div>
           <div class="contents">
             <label for="contents">contents:</label>
-            <input type="text" id="contents">
+            <input
+              type="text"
+              id="contents"
+              v-model="newPractice.contents"
+            >
           </div>
           <div class="form-footer">
             <input type="submit" value="register">
@@ -59,16 +71,45 @@ export default {
   data: function() {
     return {
       isShownForm :false,
-      practices: []
+      practices: [],
+      newPractice: {
+        title: "",
+        description: "",
+        contents: ""
+      }
     };
   },
   created: function() {
     this.getPracticeModels();
   },
+  mounted: function() {
+    const token = document.querySelector("meta[name=csrf-token]").getAttribute("content");
+    Axios.defaults.headers['X-CSRF-TOKEN'] = token;    
+  },
   methods: {
     getPracticeModels: function() {
       Axios.get("/api/practice/practice/index3").then(res => {
         this.practices = res.data.practices;
+      });
+    },
+    registerNewData: function() {
+      if(!this.newPractice.title && !this.newPractice.description && !this.newPractice.contents){
+        this.isShownForm = false;
+        return;
+      }
+
+      //よくわからんがObjectのままだと上手くいかない
+      const params = new URLSearchParams();
+      Object.entries(this.newPractice).forEach(([param, value]) =>{
+        params.append(param, value);
+      });
+    
+      Axios.post("/api/practice/practice/create", params).then(res => {
+        this.practices.push(Object.create(this.newPractice));
+        this.isShownForm = false;
+        console.log("succeeded!", res);
+      }).catch(error => {
+        console.log(error)
       });
     },
     openForm: function() {
