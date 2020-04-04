603928b7e6504db6ebadacfdf871275e101356b4
Fuga-Kawamata <f.maple.river@gmail.com>
2020-04-04 17:02:59 +0900

    [feat] Add register window UI (function is not yet)

--- app/javascript/roots/PracticeIndex3.vue
+++ app/javascript/roots/PracticeIndex3.vue
@@ -14,6 +14,32 @@
         <td>{{practice.contents}}</td>
       </tr>
     </table>
+
+    <div class="popup-cover">
+      <div class="popup-window">
+        <form action="">
+          <div class="form-header">
+            <h3 class="form-title">Register New Data</h3>
+            <button type="button" class="form-close-button">close</button>
+          </div>
+          <div class="title">
+            <label for="title">title:</label>
+            <input type="text" id="title">
+          </div>
+          <div class="description">
+            <label for="description">description:</label>
+            <input type="text" id="description">
+          </div>
+          <div class="contents">
+            <label for="contents">contents:</label>
+            <input type="text" id="contents">
+          </div>
+          <div class="form-footer">
+            <input type="submit" value="register">
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -49,5 +75,42 @@ export default {
 .practices-table td {
   border: solid 1px black;
   padding: 0.5rem;
+}
+
+.popup-cover {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.411);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.popup-window {
+  width: 70%;
+  background-color: #fff;
+
+  padding: 1rem;
+}
+.form-header {
+  display: grid;
+  grid-template-columns: 10% 80% 10%;
+  justify-items: center;
+}
+.form-title {
+  grid-column: 2;
+  margin :0.1rem;
+}
+.form-close-button {
+  grid-column: 3;
+}
+.form-footer {
+  display: grid;
+  justify-content: center;
+
+  margin-top: 0.5rem;
 }
 </style>
