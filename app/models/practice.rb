01f3ca34ea8779712523e17139a28010d560b63f
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-04-08 17:09:20 +0900

    editted image saving location

--- app/models/practice.rb
+++ app/models/practice.rb
@@ -8,11 +8,11 @@ class Practice < ApplicationRecord
       type = prefix.sub(/(image|application)(\/)/, '')
       data = Base64.decode64(image.sub(/data:#{prefix};base64,/, ''))
       filename = "#{Time.zone.now.strftime('%Y%m%d%H%M%S%L')}.#{type}"
-      File.open("#{Rails.root}/public/packs/media/images/#{filename}", 'wb') do |f|
+      File.open("#{Rails.root}/app/assets/images/#{filename}", 'wb') do |f|
         f.write(data)
       end
       eyecatch.detach if eyecatch.attached?
-      eyecatch.attach(io: File.open("#{Rails.root}/public/packs/media/images/#{filename}"), filename: filename)
+      eyecatch.attach(io: File.open("#{Rails.root}/app/assets/images/#{filename}"), filename: filename)
       #FileUtils.rm("#{Rails.root}/tmp/#{filename}")
     end
   end
