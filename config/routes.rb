2f124402acddc15b7dc0227c75193de7cf51f60a
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-04-20 22:14:51 +0900

    did setup after devise:install

--- config/routes.rb
+++ config/routes.rb
@@ -13,5 +13,8 @@ Rails.application.routes.draw do
   get 'practice/index'
   get 'practice/index2'
   get 'practice/index3'
+
+  # とりあえず
+  root to: "practice#index3"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
