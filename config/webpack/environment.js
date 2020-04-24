db77d2ad09f82f5750c4be729847392918953d42
Fuga-Kawamata <f.maple.river@gmail.com>
2020-04-24 22:48:56 +0900

    [config] Activate splitchunk

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -6,4 +6,7 @@ const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 environment.plugins.prepend('HardSourceWebpackPlugin', new HardSourceWebpackPlugin());
+
+environment.splitChunks()
+
 module.exports = environment
