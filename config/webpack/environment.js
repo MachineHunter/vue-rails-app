ab8514a2c5143ff80a0858e4913526b2776ea906
Fuga-Kawamata <f.maple.river@gmail.com>
2020-04-24 22:35:36 +0900

    [config] Install hard-source-webpack-plugin

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,9 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+environment.plugins.prepend('HardSourceWebpackPlugin', new HardSourceWebpackPlugin());
 module.exports = environment
