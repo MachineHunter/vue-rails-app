2509b473029f9d145253790daa5723aad4d19681
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-04-28 00:16:57 +0900

    added dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one :status
-  has_one :avatar
+  has_one :status, dependent: :destroy 
+  has_one :avatar, dependent: :destroy
 
   validates :email, email: true
 
