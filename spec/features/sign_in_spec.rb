333871cfc717d24f618842ad9f0b239f064ef03c
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-04-27 15:09:26 +0900

    corrected rubocop error

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,22 +1,22 @@
-#require 'rails_helper'
+# require 'rails_helper'
 
-#RSpec.feature 'Sign In', type: :feature do
-  #given!(:user) { create(:user) }
-  #given(:wrong_email) { 'wrong@gmail.com' }
-  #given(:wrong_password) { 'wrongpass' }
+# RSpec.feature 'Sign In', type: :feature do
+# given!(:user) { create(:user) }
+# given(:wrong_email) { 'wrong@gmail.com' }
+# given(:wrong_password) { 'wrongpass' }
 
-  #scenario 'normal sign in' do
-    #sign_in_with_factory(user)
-    #expect(current_path).to eq root_path
-  #end
+# scenario 'normal sign in' do
+# sign_in_with_factory(user)
+# expect(current_path).to eq root_path
+# end
 
-  #scenario 'informat email' do
-    #sign_in_without_factory(wrong_email, user.password)
-    #expect(page).to have_content '無効な形式です'
-  #end
+# scenario 'informat email' do
+# sign_in_without_factory(wrong_email, user.password)
+# expect(page).to have_content '無効な形式です'
+# end
 
-  #scenario 'informat password' do
-    #sign_in_without_factory(user.email, wrong_password)
-    #expect(page).to have_content '無効な形式です'
-  #end
-#end
+# scenario 'informat password' do
+# sign_in_without_factory(user.email, wrong_password)
+# expect(page).to have_content '無効な形式です'
+# end
+# end
