41017fd4d61ca38cb5765f8c2dd7975a8daf5948
Fuga-Kawamata <f.maple.river@gmail.com>
2020-04-28 23:39:06 +0900

    [update] Add validation

--- app/javascript/packs/usersEdit.js
+++ app/javascript/packs/usersEdit.js
@@ -2,5 +2,11 @@ const button = document.getElementById("button-for-delete")
 const formForDelete = document.formfordelete
 
 button.addEventListener("click", () => {
-  formForDelete.submit()
+  const elems = formForDelete.getElementsByClassName("needs-validation");
+  Array.prototype.filter.call(elems, function(elem){
+    elem.classList.add('was-validated');
+  });
+  if (formForDelete.checkValidity() === true) {
+    formForDelete.submit()
+  }
 })
