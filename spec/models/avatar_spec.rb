fd62a62d8e5b900daa4ea2d3858f6df47cdb9d6d
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-04-29 02:07:29 +0900

    model test with factory succeeded

--- spec/models/avatar_spec.rb
+++ spec/models/avatar_spec.rb
@@ -3,7 +3,13 @@ require 'rails_helper'
 RSpec.describe Avatar, type: :model do
   let(:avatar) { create(:avatar) }
 
-  it do
-    binding.pry
+  describe 'データ整合性チェック' do
+    it '正しくfactoryで作られてるか' do
+      expect(avatar.filename).to eq "white.jpeg"
+    end
+
+    it 'ユーザーと結び付いているか' do
+      expect(avatar.user_id).not_to eq nil
+    end
   end
 end
