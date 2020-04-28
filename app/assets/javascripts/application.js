db49c8b37a8d4da073145b612fef22ac029907bd
Fuga-Kawamata <f.maple.river@gmail.com>
2020-04-28 16:51:44 +0900

    [update] Let selected file name display on form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,3 +37,18 @@ const bsSubmitListenerForValidation = () => {
 
 window.addEventListener('load', bsSubmitListenerForValidation, false);
 window.addEventListener('turbolinks:load', bsSubmitListenerForValidation, false);
+
+const fileChangeListener = () => {
+  const input = document.getElementById("icon-input");
+  const label = document.getElementById("field-for-icon-input");
+  if(input && label){
+    input.addEventListener("change", function(event) {
+      const fileFakePath = event.target.value;
+      const fileName = fileFakePath.split(/\\|\//).slice(-1)[0];
+      label.textContent = fileName;
+    }, false);
+  }
+}
+
+window.addEventListener('load', fileChangeListener, false);
+window.addEventListener('turbolinks:load', fileChangeListener, false);
