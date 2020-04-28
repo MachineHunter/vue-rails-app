97970b20c7c134e45f6aea3918342c88c75f67fb
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-04-29 02:08:12 +0900

    runned rubocop

--- spec/models/avatar_spec.rb
+++ spec/models/avatar_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Avatar, type: :model do
 
   describe 'データ整合性チェック' do
     it '正しくfactoryで作られてるか' do
-      expect(avatar.filename).to eq "white.jpeg"
+      expect(avatar.filename).to eq 'white.jpeg'
     end
 
     it 'ユーザーと結び付いているか' do
