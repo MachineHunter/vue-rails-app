7c8537902697568570da73390c343ef935160f88
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-04-29 22:14:56 +0900

    added api to update avatar

---
+++ app/controllers/api/avatar_controller.rb
@@ -0,0 +1,23 @@
+module Api
+  class AvatarController < ApplicationController
+    def index
+      image = current_user.avatar.image
+      filetype = current_user.avatar.filetype
+      send_data image, type: filetype, disposition: 'inline'
+    end
+
+    def update
+      image = params[:avatar]
+      @avatar = current_user.avatar
+      @avatar.filename = avatar.filename
+      @avatar.filetype = avatar.content_type
+      @avatar.image    = avatar.tempfile.read
+      if @avatar.save
+        redirect_to root_path 
+      else
+        response_bad_request
+      end
+    end
+
+  end
+end
--- app/controllers/api/user_pages_controller.rb
+++ app/controllers/api/user_pages_controller.rb
@@ -4,10 +4,5 @@ module Api
       @user = current_user
     end
 
-    def avatar
-      image = current_user.avatar.image
-      filetype = current_user.avatar.filetype
-      send_data image, type: filetype, disposition: 'inline'
-    end
   end
 end
