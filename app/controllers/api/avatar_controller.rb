35c4bdb8af97d6f8379009abae8f457062df12e8
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-04-30 18:57:17 +0900

    added correct_format func

--- app/controllers/api/avatar_controller.rb
+++ app/controllers/api/avatar_controller.rb
@@ -8,15 +8,32 @@ module Api
 
     def update
       image = params[:avatar]
+      correct_format(image)
+
       @avatar = current_user.avatar
       @avatar.filename = image.original_filename
       @avatar.filetype = image.content_type
       @avatar.image    = image.tempfile.read
+
+      binding.pry
+
       if @avatar.save
         redirect_to root_path
       else
         response_bad_request
       end
     end
+
+    private
+
+    def correct_format(image)
+      name = image.original_filename.split(".").first
+      prefix = image.original_filename.split(".").last
+      type = image.content_type.split("/").last
+      return if prefix==type
+
+      image.original_filename = "#{name}.#{type}"
+    end
+
   end
 end
