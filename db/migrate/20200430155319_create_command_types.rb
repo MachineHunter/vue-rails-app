533ec5fabb58ef7eebe1d8c2af1ce880e8ac2d7f
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-05-01 04:30:26 +0900

    added description column

--- db/migrate/20200430155319_create_command_types.rb
+++ db/migrate/20200430155319_create_command_types.rb
@@ -2,6 +2,7 @@ class CreateCommandTypes < ActiveRecord::Migration[5.2]
   def change
     create_table :command_types do |t|
       t.text :name
+      t.text :description
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,17 @@
 Practice.create(id: 1, title: "first title", description: "first description", contents: "hello first")
 Practice.create(id: 2, title: "second title", description: "second description", contents: "hello second")
 Practice.create(id: 3, title: "third title", description: "third description", contents: "hello third")
+
+CommandType.create(id:1, name: "")
+CommandType.create(id:2, name: "")
+CommandType.create(id:3, name: "")
+CommandType.create(id:4, name: "")
+
+Genre.create(id:1, name: "linux")
+Genre.create(id:2, name: "vim")
+Genre.create(id:3, name: "python")
+Genre.create(id:4, name: "go")
+Genre.create(id:5, name: "powershell")
+Genre.create(id:6, name: "cmd")
+Genre.create(id:7, name: "ruby")
+Genre.create(id:8, name: "git")
