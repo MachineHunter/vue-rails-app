71a2b4a2f0ec6499121b70ce655100357d47bbee
Fuga-Kawamata <f.maple.river@gmail.com>
2020-04-30 20:55:24 +0900

    [add] Add validation to avatar input & Integrate function

--- app/javascript/packs/usersEdit.js
+++ app/javascript/packs/usersEdit.js
@@ -12,16 +12,3 @@ const fileChangeListener = () => {
 
 window.addEventListener('turbolinks:load', fileChangeListener, false);
 
-
-const button = document.getElementById("button-for-delete")
-const formForDelete = document.formfordelete
-
-button.addEventListener("click", () => {
-  const elems = formForDelete.getElementsByClassName("needs-validation");
-  Array.prototype.filter.call(elems, function(elem){
-    elem.classList.add('was-validated');
-  });
-  if (formForDelete.checkValidity() === true) {
-    formForDelete.submit()
-  }
-})
