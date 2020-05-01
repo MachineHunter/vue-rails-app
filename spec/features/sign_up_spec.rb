0ee797b3ebcb30433486564612c9395376bf467f
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-05-02 02:02:55 +0900

    sign_in feature spec succeeded

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,20 @@
+# require 'rails_helper'
+
+# RSpec.feature 'Sign Up', type: :feature do
+# before do
+# Capybara.current_driver = :selenium_chrome_headless
+# end
+
+# given(:image) { "#{Rails.root}/spec/factories/images/eagle.jpg" }
+
+# scenario 'normal sign up' do
+# sign_up('tester', 'test@gmail.com', 'testtest', 'testtest', image)
+# expect(current_path).to eq root_path
+# end
+
+# #scenario 'informat email' do
+# #visit new_user_registration_path
+# #sign_up('tester', '@gmail.com', 'testtest', 'testtest', image)
+# #expect(page).to have_content '無効な形式です'
+# #end
+# end
---
+++ spec/support/feature_devise_helper.rb
@@ -0,0 +1,35 @@
+module FeatureDevise
+  def sign_in_without_factory(email, password)
+    visit new_user_session_path
+    fill_in 'user[email]', with: email
+    fill_in 'user[password]', with: password
+    click_button 'Log in'
+  end
+
+  def sign_in_with_factory(user)
+    visit new_user_session_path
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    click_button 'Log in'
+  end
+
+  def sign_up(name, email, password, confirmation)
+    visit new_user_registration_path
+    fill_in 'user[name]', with: name
+    fill_in 'user[email]', with: email
+    fill_in 'user[password]', with: password
+    fill_in 'user[password_confirmation]', with: confirmation
+    click_button 'Sign up'
+  end
+
+  # def account_update(name, email, new_password, new_confirmation, old_password, avatar)
+  # visit edit_user_registration_path
+  # fill_in 'user[name]', with: name
+  # fill_in 'user[email]', with: email
+  # fill_in 'user[password]', with: new_password
+  # fill_in 'user[password_confirmation]', with: new_confirmation
+  # fill_in 'user[current_password]', with: old_password
+  # attach_file 'dummy', avatar
+  # click_button 'Update'
+  # end
+end
