cc69bced681a59a576bfada9aba1f42409999b7e
Fuga-Kawamata <f.maple.river@gmail.com>
2020-05-01 17:21:51 +0900

    [feat] Display commands on table using pager for now

--- app/javascript/components/UserCommands.vue
+++ app/javascript/components/UserCommands.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="user-commands" class="main-center">
-    コマンド一覧ページ
+    <h2>コマンド一覧</h2>
+    
+    <div class="overflow-auto">
+      <b-table
+        id="commands-table"
+        :items="commands"
+        :fields="fields"
+        :per-page="perPage"
+        :current-page="currentPage"
+        small
+      />
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        aria-controls="commands-table"
+      />
+    </div>
   </div>
 </template>
 
@@ -10,6 +27,18 @@ export default {
     commands: {
       type: Array,
       required: true
+    }
+  },
+  data: function() {
+    return {
+      fields: ["title", "description", "user_id", "genre_id", "command_type_id"],
+      currentPage: 1,
+      perPage: 5
+    }
+  },
+  computed: {
+    rows() {
+      return this.commands.length
     }
   }
 }
