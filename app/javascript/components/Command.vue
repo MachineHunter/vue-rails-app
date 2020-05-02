e609825984dafc15c12c2bdb600b317f555403d7
Fuga-Kawamata <f.maple.river@gmail.com>
2020-05-02 13:53:04 +0900

    [update] Change style so that long text will be cut & Add test data for this

--- app/javascript/components/Command.vue
+++ app/javascript/components/Command.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
     <b-card>
-      <b-media>
-        <template v-slot:aside>
+      <b-media no-body>
+        <b-media-aside>
           <b-img blank blank-color="#ccc" width="64" alt="placeholder"></b-img>
-        </template>
-        <div class="d-flex justify-content-between">
-          <h5 class="mt-0 mb-1">{{command.title}}</h5>
-          <div>
-            <b-badge class="px-2 py-1">linux</b-badge>
-            <b-badge class="px-2 py-1">自動化系</b-badge>
+        </b-media-aside>
+        <b-media-body class="command-body pl-3">
+          <div class="d-flex justify-content-between align-items-center mb-1">
+            <h5 class="command-title mb-0 py-1">{{command.title}}</h5>
+            <div class="d-flex justify-content-end align-items-center badges-sm-direction">
+              <b-badge class="px-2 py-1 ml-2 h-auto badge-sm-mb">linux</b-badge>
+              <b-badge class="px-2 py-1 ml-2 h-auto">自動化系</b-badge>
+            </div>
           </div>
-        </div>
-        <hr>
-        <p class="mb-0">
-          {{command.description}}
-        </p>
+          <hr class="my-1">
+          <p class="mb-0 command-description">
+            {{command.description}}
+          </p>
+        </b-media-body>
       </b-media>
 
     </b-card>
@@ -35,4 +37,28 @@ export default {
 </script>
 
 <style scoped>
+.command-body {
+  width: calc(100% - 64px);
+}
+.command-title {
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+}
+.command-description {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
+}
+@media (max-width: 576px) {
+  .badges-sm-direction {
+    flex-direction: column;
+    align-items: stretch !important;
+  }
+  .badge-sm-mb {
+    margin-bottom: 0.3rem;
+  }
+
+}
 </style>
