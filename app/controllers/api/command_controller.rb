a1b730ad622916f80821c7bd3cc1a34608bb5010
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-05-05 13:09:46 +0900

    made create api of command

--- app/controllers/api/command_controller.rb
+++ app/controllers/api/command_controller.rb
@@ -3,5 +3,27 @@ module Api
     def index
       @commands = current_user.command.all
     end
+
+    def create
+      params[:zipdata] = params[:zipdata].tempfile.read
+      @command = Command.new(command_params)
+      @command.command_file = CommandFile.new(command_file_params)
+
+      if @command.command_file.save && @command.save
+        redirect_to command_pages_index_path 
+      else
+        response_bad_request
+      end
+    end
+
+    private
+    
+    def command_params
+      params.permit(:title, :description, :genre_id, :command_type_id)
+    end
+
+    def command_file_params
+      params.permit(:filename, :zipdata)
+    end
   end
 end
