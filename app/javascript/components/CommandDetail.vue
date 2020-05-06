e769b33bc578d4aa1d744b72a9c26e241d147f17
Fuga-Kawamata <f.maple.river@gmail.com>
2020-05-06 15:15:12 +0900

    [feat] Get command data & Show it for now

--- app/javascript/components/CommandDetail.vue
+++ app/javascript/components/CommandDetail.vue
@@ -1,15 +1,51 @@
 <template>
-  <div id="command-detail">
-    {{commandId}}
+  <div id="command-detail" class="main-center">
+    <h2>{{command.title}}</h2>
+    <b-badge>genre{{command.genre_id}}</b-badge>
+    <b-badge>type{{command.command_type_id}}</b-badge>
+
+    <br/>
+    <h5>{{fileName}}</h5>
+    <p>{{fileTree}}</p>
+    <p>{{fileContents}}</p>
+
+    <br/>
+    <p>description</p>
+    <p>{{command.description}}</p>
   </div>
 </template>
 
 <script>
+import Axios from "axios"
+
 export default {
   props: {
     commandId: {
       type: Number,
       required: true
+    }
+  },
+  data: function() {
+    return {
+      command: {},
+      fileName: "",
+      fileTree: {},
+      fileContents: {}
+    }
+  },
+  created: function() {
+    this.getCommandData()
+  },
+  methods: {
+    getCommandData: function() {
+      Axios.get(`/api/command/${this.commandId}`).then(res => {
+        this.command = res.data.command
+        this.fileName = res.data.filename
+        this.fileTree = res.data.filetree
+        this.fileContents = res.data.filedatas
+      }).catch(err => {
+        console.log(err);
+      })
     }
   }
 }
