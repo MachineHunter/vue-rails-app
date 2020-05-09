6936ca6c1375af425c7776942587070bbd59ea64
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-05-09 21:08:43 +0900

    added destroy spec

--- spec/requests/api/command_spec.rb
+++ spec/requests/api/command_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe 'Command', type: :request do
   let(:send_index_request) { get api_command_index_path }
   let(:send_new_request) { get new_api_command_path }
   let(:send_show_request) { get api_command_path command.id }
+  let(:send_destroy_request) { delete api_command_path command.id }
   let(:result) { JSON.parse(response.body, { symbolize_names: true }) }
 
   before do
@@ -37,6 +38,14 @@ RSpec.describe 'Command', type: :request do
       expect(result[:filetree][:name]).to eq 'test'
       expect(result[:command][:title]).to eq 'test_command'
       expect(result[:filedatas].nil?).to eq false
+    end
+  end
+
+  describe 'destroy' do
+    it do
+      expect do
+        send_destroy_request
+      end.to change { CommandFile.count }.by(-1)
     end
   end
 end
