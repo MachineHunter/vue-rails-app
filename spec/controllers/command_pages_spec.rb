0b04ceb409900a17ccb009b5ef9cdff5266ec64b
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-05-09 21:25:16 +0900

    tried to make command_pages_spec controller spec

---
+++ spec/controllers/command_pages_spec.rb
@@ -0,0 +1,18 @@
+# require 'rails_helper'
+
+# Rspec.describe 'CommandPages', type: :request do
+# let!(:command) { create(:command) }
+# let(:user) { command.user }
+# let(:download) { get command_pages_index_path command.id }
+
+# before do
+# login user
+# end
+
+# describe 'download' do
+# it do
+# end
+# end
+
+# end
+# ログインしてなくてもできるようにまずする！
