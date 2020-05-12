dbf658ad7af2258f11764ad853412dcd7041cb48
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-05-12 21:20:24 +0900

    id=0 is profile

--- app/controllers/api/user_pages_controller.rb
+++ app/controllers/api/user_pages_controller.rb
@@ -1,7 +1,11 @@
 module Api
   class UserPagesController < ApplicationController
     def index
-      @user = User.find(params[:id])
+      @user = if params[:id].zero?
+                current_user
+              else
+                User.find(params[:id])
+              end
       @status = @user.status
     end
   end
