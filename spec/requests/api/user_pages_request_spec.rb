8f0e0456fc52254e56d45e6ae73387e44e2cddeb
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-05-13 22:17:28 +0900

    corrected user_pages_request_spec

--- spec/requests/api/user_pages_request_spec.rb
+++ spec/requests/api/user_pages_request_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'UserPages', type: :request do
   let!(:user) { create(:user) }
-  let(:send_index_request) { get api_user_pages_index_path }
+  let!(:user2) { create(:user) }
+  let(:send_index_request_own) { get api_user_pages_index_path 0 }
+  let(:send_index_request_other) { get api_user_pages_index_path user2.id }
   let(:result) { JSON.parse(response.body, { symbolize_names: true }) }
 
   before do
@@ -10,10 +12,16 @@ RSpec.describe 'UserPages', type: :request do
   end
 
   describe 'index' do
-    it do
-      send_index_request
+    it 'own profile' do
+      send_index_request_own
       expect(response).to have_http_status(200)
-      expect(result[:user][:name]).to eq('specuser')
+      expect(result[:user][:name]).to eq(user.name)
+    end
+
+    it 'other profile' do
+      send_index_request_other
+      expect(response).to have_http_status(200)
+      expect(result[:user][:name]).to eq(user2.name)
     end
   end
 end
