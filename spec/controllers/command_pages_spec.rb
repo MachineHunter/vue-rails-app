ac226d77b8526f69e4a8450a8a484d993f5f74b1
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-05-13 22:46:38 +0900

    2 spec waiting for view

--- spec/controllers/command_pages_spec.rb
+++ spec/controllers/command_pages_spec.rb
@@ -1,18 +1,17 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# Rspec.describe 'CommandPages', type: :request do
-# let!(:command) { create(:command) }
-# let(:user) { command.user }
-# let(:download) { get command_pages_index_path command.id }
+Rspec.describe 'CommandPages', type: :request do
+  # let!(:command) { create(:command) }
+  # let(:user) { command.user }
+  # let(:download) { get command_pages_index_path command.id }
+  pending 'あとで書く'
 
-# before do
-# login user
-# end
+  # before do
+  # login user
+  # end
 
-# describe 'download' do
-# it do
-# end
-# end
-
-# end
-# ログインしてなくてもできるようにまずする！
+  # describe 'download' do
+  # it do
+  # end
+  # end
+end
