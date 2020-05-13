44a296d944bbdc74ffcdd00c12181da3d1ec2b99
Fuga-Kawamata <f.maple.river@gmail.com>
2020-05-13 18:32:26 +0900

    [Extract commmon mount process as commonMounter.js]

--- app/javascript/packs/commandPagesIndex.js
+++ app/javascript/packs/commandPagesIndex.js
@@ -1,24 +1,7 @@
-import Vue from 'vue'
+import mounter from "./commonMounter"
 import CommandPagesIndex from '../roots/CommandPagesIndex.vue'
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-import "./custom.scss"
-
-import TurbolinksAdapter from 'vue-turbolinks'
-Vue.use(TurbolinksAdapter)
-
-const vueMountListener = () => {
-  const vueHook = document.getElementById("vue-hook")
-  if(vueHook){
-    const currentUserId = Number(vueHook.dataset.current_user_id)
-    const commandPagesIndex = new Vue({
-      render: h => h(CommandPagesIndex, {props: {currentUserId}})
-    }).$mount()
-    document.body.replaceChild(commandPagesIndex.$el, vueHook)
-  }
-
-}
-
-document.addEventListener('turbolinks:load', vueMountListener)
+mounter(CommandPagesIndex, (hook) => {
+  const currentUserId = Number(hook.dataset.current_user_id)
+  return {currentUserId}
+})
