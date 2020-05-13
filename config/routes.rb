48e90bc128c3fc760d952ae626652f72ce540544
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-05-14 03:37:27 +0900

    user_page profile index => show

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,7 @@ Rails.application.routes.draw do
       delete 'practice/destroy'
     end
 
-    scope :user_pages do
-      get 'index/:id', to: 'user_pages#index', as: 'user_pages_index'
-    end
+    resources :user_pages, only: [:show]
 
     scope :avatar do
       get 'index/:id', to: 'avatar#index', as: 'avatar_index'
@@ -31,12 +29,12 @@ Rails.application.routes.draw do
   get 'practice/index2'
   get 'practice/index3'
 
-  get 'user_pages/index' 
+  get 'user_pages/show' 
   get 'command_pages/index'
   resources :command_pages, only: [:index, :new, :show] do
     get 'download/:id', to: 'command_pages#download', on: :collection, as: 'download'
   end
 
-  root to: "user_pages#index"
+  root to: "user_pages#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
