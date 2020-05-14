6a122140adfa6893fdfbf995d485c796077c8448
KazuMatsuHack9981 <kazu3matsu@gmail.com>
2020-05-14 23:01:58 +0900

    deleted practice routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,30 +4,18 @@ Rails.application.routes.draw do
   }
 
   namespace :api, format: 'json' do
-    namespace :practice do
-      get 'practice/index/:id', to: 'practice#index'
-      get 'practice/index3'
-      get "practice/image_show"
-      post 'practice/create'
-      put 'practice/update'
-      delete 'practice/destroy'
-    end
-
     resources :user_pages, only: [:show]
 
     scope :avatar do
       get 'index/:id', to: 'avatar#index', as: 'avatar_index'
     end
+
     namespace :avatar do
       post 'update'
     end
 
     resources :command
   end
-
-  get 'practice/index'
-  get 'practice/index2'
-  get 'practice/index3'
 
   resources :user_pages, only: [:show]
 
@@ -37,5 +25,4 @@ Rails.application.routes.draw do
   end
 
   root to: "command_pages#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
