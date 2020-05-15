7fe9bd7ec243d2d0deddbf7e3fdbe7abe39990f9
Fuga-Kawamata <f.maple.river@gmail.com>
2020-05-16 00:19:57 +0900

    [feat] Add filter and sort except for user

--- app/javascript/components/MainCommandList.vue
+++ app/javascript/components/MainCommandList.vue
@@ -14,6 +14,35 @@
     
     <b-overlay :show="noCommands" rounded="sm">
       <div>
+        <h4>filter</h4>
+        <b-form-group label="genre">
+          <b-form-checkbox-group
+            v-model="filterOptions.genreIds"
+            :options="genreOptions"
+          />
+        </b-form-group>
+        
+        <b-form-group label="command type">
+          <b-form-checkbox-group
+            v-model="filterOptions.commandTypeIds"
+            :options="commandTypeOptions"
+          />
+        </b-form-group>
+        <h4>sort</h4>
+        <b-form-group>
+          <b-form-radio-group v-model="sortOptions.key">
+            <b-form-radio value="id">id</b-form-radio>
+            <b-form-radio value="title">title</b-form-radio>
+          </b-form-radio-group>
+        </b-form-group>
+        <b-form-group>
+          <b-form-radio-group v-model="sortOptions.ascending">
+            <b-form-radio :value="true">ascending</b-form-radio>
+            <b-form-radio :value="false">descending</b-form-radio>
+          </b-form-radio-group>
+        </b-form-group>
+      </div>
+      <div>
         <b-pagination
           v-model="currentPage"
           :total-rows="rows"
@@ -66,7 +95,18 @@ export default {
       commands: null,
       userId: 0,
       currentPage: 1,
-      perPage: 5
+      perPage: 5,
+      genres: [],
+      commandTypes: [],
+      filterOptions: {
+        userIds: [1, 2],
+        genreIds: [],
+        commandTypeIds: []
+      },
+      sortOptions: {
+        key: "id", // "id" or "title"
+        ascending: true
+      }
     }
   },
   computed: {
@@ -74,24 +114,79 @@ export default {
       if(!this.commands) return false
       return this.commands.length === 0
     },
+    genreOptions: function() {
+      return this.genres.map(genre => (
+        {
+          text: genre.name,
+          value: genre.id
+        }
+      ))
+    },
+    commandTypeOptions: function() {
+      return this.commandTypes.map(type => (
+        {
+          text: type.name,
+          value: type.id
+        }
+      ))
+    },
+    computedCommands() {
+      if(!this.commands) return []
+      const filteredCommands = this.commands.filter(command => {
+        return this.filterOptions.userIds.includes(command.owner.id)
+          && this.filterOptions.genreIds.includes(command.genre_id)
+          && this.filterOptions.commandTypeIds.includes(command.command_type_id)
+      })
+      const sortedFilteredCommands = filteredCommands.sort((command1, command2) => {
+        let result = 0
+        if(command1[this.sortOptions.key] < command2[this.sortOptions.key]) {
+          result = -1
+        } else if(command1[this.sortOptions.key] === command2[this.sortOptions.key]) {
+          result = 0
+        } else {
+          result = 1
+        }
+        return this.sortOptions.ascending ? result : (-result)
+      })
+      return sortedFilteredCommands
+    },
     rows() {
-      if(!this.commands) return 0
-      return this.commands.length
+      if(!this.computedCommands) return 0
+      return this.computedCommands.length
     },
     commandsToDisplay() {
-      if(!this.commands) return []
+      if(!this.computedCommands) return []
       const start = (this.currentPage - 1) * this.perPage
-      return [...this.commands].splice(start, this.perPage)
+      return [...this.computedCommands].splice(start, this.perPage)
     }
   },
   created: function() {
     this.getCommands()
+    this.getUserData()
+    this.getTagData()
     this.getRememberedPage()
   },
   methods: {
     getCommands: function() {
       Axios.get("/api/command").then(res => {
         this.commands = res.data.command
+      }).catch(err => {
+        console.log(err)
+      })
+    },
+    getUserData: function() {
+      // if(there_is_id_in_cookie){
+      //   this.filterOptions.userIds = [id_from_cookie]
+      // }else{
+      //   this.filterOptions.userIds = res.data.users.map(user => user.id)
+      // }
+    },
+    getTagData() {
+        Axios.get("/api/command/new").then(res => {
+        this.genres = res.data.genres
+        this.commandTypes = res.data.command_types
+        this.filterOptions.genreIds = res.data.genres.map(genre => genre.id)
+        this.filterOptions.commandTypeIds = res.data.command_types.map(commandType => commandType.id)
       }).catch(err => {
         console.log(err)
       })
